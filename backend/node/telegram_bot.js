3549e60d67fa8bda24c38462faec4f66abf4e4bd
Carlos Padilla <carlosfpm-@hotmail.com>
2024-11-17 11:10:45 -0500

    Todo corregido

--- backend/node/telegram_bot.js
+++ backend/node/telegram_bot.js
@@ -6,9 +6,19 @@ const datos = JSON.parse(fs.readFileSync('../pyhton/database/pronostico_semanal.
 // 'TOKEN'
 const bot = new TelegramBot('7634065879:AAHLn8X2XWf4LBuG7-l9EJ8GECGatK_Ru-0', { polling: true });
 
+// Palabras clave para buscar pronósticos
+const palabrasClave = ["hoy", "calidad del aire para hoy", "pronostico para hoy", "pronosticos para hoy", "pronostico del dia", "estado del aire hoy", "calidad del aire hoy", "condiciones para hoy", "prediccion para hoy", "estado ambiental del dia", "prevision de calidad hoy", "como esta el aire hoy", "aire del dia", "que tal el aire hoy", "condiciones ambientales hoy", "nivel de contaminacion hoy", "calidad ambiental actual", "pronostico ambiental diario", "aire limpio hoy", "estado del ambiente hoy", "que esperar hoy", "predicciones del aire para hoy", "situacion ambiental hoy"];
+const palabrasClavePrediccionManana = ["mañana", "manana", "calidad del aire para mañana", "pronostico para mañana", "pronosticos para mañana", "pronostico del dia siguiente", "estado del aire mañana", "calidad del aire mañana", "condiciones para mañana", "prediccion para mañana", "estado ambiental del dia siguiente", "prevision de calidad mañana", "como esta el aire mañana", "aire del dia siguiente", "que tal el aire mañana", "condiciones ambientales mañana", "nivel de contaminacion mañana", "calidad ambiental siguiente", "pronostico ambiental diario siguiente", "aire limpio mañana", "estado del ambiente mañana", "que esperar mañana", "predicciones del aire para mañana", "situacion ambiental mañana"];
+
+//Bandera para evitar errores
+var bandera = true;
+
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const welcomeMessage = `¡Hola ${fullName}! Soy AIre bienestar, tu asistente que te ayuda a preparate para respirar tranquilamente en la ciudad. \n\n ¿Cómo puedo ayudarte? Prueba preguntar por los pronósticos de calidad de aire.`;
+    const userName = msg.from.first_name || 'Usuario'; // Nombre del usuario
+    const userLastName = msg.from.last_name || ''; // Apellido (si está disponible)
+    const fullName = `${userName} ${userLastName}`.trim(); // Nombre completo
+    const welcomeMessage = `¡Hola ${fullName}! Soy AIre bienestar, tu asistente que te ayuda a preparate para respirar tranquilamente en la ciudad de Cali. \n\n ¿Cómo puedo ayudarte? Prueba preguntar por los pronósticos de calidad de aire de hoy o mañana.`;
     bot.sendMessage(chatId, welcomeMessage);
 });
 
@@ -24,8 +34,6 @@ bot.on('message', (msg) => {
     const userName = msg.from.first_name || 'Usuario'; // Nombre del usuario
     const userLastName = msg.from.last_name || ''; // Apellido (si está disponible)
     const fullName = `${userName} ${userLastName}`.trim(); // Nombre completo
-    const palabrasClave = ["calidad del aire para hoy", "pronostico para hoy", "pronosticos para hoy", "pronostico del dia", "estado del aire hoy", "calidad del aire hoy", "condiciones para hoy", "prediccion para hoy", "estado ambiental del dia", "prevision de calidad hoy", "como esta el aire hoy", "aire del dia", "que tal el aire hoy", "condiciones ambientales hoy", "nivel de contaminacion hoy", "calidad ambiental actual", "pronostico ambiental diario", "aire limpio hoy", "estado del ambiente hoy", "que esperar hoy", "predicciones del aire para hoy", "situacion ambiental hoy"];
-    const palabrasClavePrediccionManana = ["calidad del aire para mañana", "pronostico para mañana", "pronosticos para mañana", "pronostico del dia siguiente", "estado del aire mañana", "calidad del aire mañana", "condiciones para mañana", "prediccion para mañana", "estado ambiental del dia siguiente", "prevision de calidad mañana", "como esta el aire mañana", "aire del dia siguiente", "que tal el aire mañana", "condiciones ambientales mañana", "nivel de contaminacion mañana", "calidad ambiental siguiente", "pronostico ambiental diario siguiente", "aire limpio mañana", "estado del ambiente mañana", "que esperar mañana", "predicciones del aire para mañana", "situacion ambiental mañana"];
 
     const text = msg.text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");;
   
@@ -42,15 +50,11 @@ bot.on('message', (msg) => {
           `${resultado.detalle}\n` +
           `${resultado.recomendaciones}`
         );
+        bandera = false;
       } else {
         bot.sendMessage(chatId, `Hola ${fullName}, lo siento, no encontré pronósticos para hoy (${fechaHoy}).`);
       }
-    } else {
-      bot.sendMessage(chatId, `${userName}, te puedo dar informacion a cerca de la calidad del aire en la ciudad de Cali, prueba enviandome un mensaje con algo como "¿Calidad del aire para hoy?" o "Pronostico para hoy sobre la calidad del aire".`);
-    }
-
-    // Responder si el mensaje contiene palabras clave
-    if (palabrasClavePrediccionManana.some((palabra) => text.includes(palabra))) {
+    } else if (palabrasClavePrediccionManana.some((palabra) => text.includes(palabra))) {
       const fechaHoy = new Date().toISOString().split('T')[0]; // Fecha actual en formato YYYY-MM-DD
 
       // Convertir fechaHoy a un objeto Date
@@ -72,12 +76,33 @@ bot.on('message', (msg) => {
           `${resultado.detalle}\n` +
           `${resultado.recomendaciones}`
         );
+        bandera = false;
       } else {
         bot.sendMessage(chatId, `Hola ${fullName}, lo siento, no encontré pronósticos para hoy (${fechaHoy}).`);
       }
-    } else {
-      bot.sendMessage(chatId, `${userName}, te puedo dar informacion a cerca de la calidad del aire en la ciudad de Cali, prueba enviandome un mensaje con algo como "¿Calidad del aire para hoy?" o "Pronostico para hoy sobre la calidad del aire".`);
     }
+});
+
+// Manejar errores
+bot.on('polling_error', (error) => {
+  console.log(error);
+});
+
+// Respuesta a mensajes diferentes a los comandos y palabras clave
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const userName = msg.from.first_name || 'Usuario'; // Nombre del usuario
+  const userLastName = msg.from.last_name || ''; // Apellido (si está disponible)
+  const fullName = `${userName} ${userLastName}`.trim(); // Nombre completo
+  const allKeywords = palabrasClave.concat(palabrasClavePrediccionManana);
+
+  if ((msg.text != '/start') && (!allKeywords.includes(msg.text.toLowerCase())) && bandera) {
+    const message = `Hola ${fullName}, lo siento, no entendí tu mensaje. ¿Podrías intentar con palabras como "hoy" o "mañana"?`;
+    sendMessage(chatId, message);
+  }
+
+  bandera = true;
+
 });
 
 // Función para enviar un mensaje
